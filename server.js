6c90b730b52dbab0e4fe160a8f9333e1d3798914
Dani Carter <carte470@d.umn.edu>
2018-03-11 17:50:14 -0500

    updated package.json file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+var db = require("./models");
+
 app.use(express.static('public'));
 
 // Sets up the Express app to handle data parsing
@@ -17,10 +19,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/coffeeController.js");
+require("./controllers/coffeeController.js")(app);
 
-app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// app.use(routes);
+db.sequelize.sync({ force:true }).then(function() {
+	app.listen(PORT, function() {
+  		console.log("App now listening at localhost:" + PORT);
+	});
 });
