d5b830a200f49fb559c064552b51826d716ac4be
Dani Carter <carte470@d.umn.edu>
2018-03-10 22:41:17 -0600

    updated config.json, routes and models

--- controllers/coffeeController.js
+++ controllers/coffeeController.js
@@ -1,7 +1,58 @@
-var express = require("express");
-// Import the model  to use its database functions.
-var coffee = require("../models/coffee.js");
-var router = express.Router();
+var db = require("..models");
+module.exports = function(app) {
+
+app.get("/", function(req, res) {
+	db.Coffee.findAll({}).then(function(dbCoffee) {
+		res.json(dbCoffee);
+	});
+});
+
+app.post("/api/coffee", function(req,res) {
+	console.log(req.body);
+	db.Coffee.create({
+		name:req.body.text,
+		drank: req.body.drank
+	}).then(function(dbCoffee) {
+		res.json(dbCoffee);
+	});
+});
+
+app.put("/api/coffee/:id", function(req, res) {
+	db.Coffee.update({
+		name:req.body.name,
+		drank:req.body.drank
+	}, {
+		where: {
+			id: req.body.id
+		}
+	}).then(function(dbCoffee) {
+		res.json(dbCoffee);
+	});
+});
+
+app.delete("/api/coffee/:id", function(req, res) {
+	db.Coffee.destroy({
+		where: {
+			id: req.params.id
+		}
+	}).then(function(dbCoffee) {
+		res.json(dbCoffee);
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 router.get("/", function(req, res) {
 	coffee.all(function(data) {
 		var hbsObject = {
--- models/coffee.js
+++ models/coffee.js
@@ -1,25 +1,7 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
-var coffee = {
-	all: function(cb) {
-		orm.all("coffee", function(res) {
-			cb(res);
-		});
-	},
-	create: function(cols, vals, cb) {
-		orm.create("coffee", cols, vals, function(res) {
-			cb(res);
-		});
-	}, 
-	update: function(objColVals, condition, cb) {
-		orm.update("coffee", objColVals, condition, function(res) {
-			cb(res);
-		});
-	},
-	delete: function(condition, cb) {
-		orm.delete("coffee", condition, function(res) {
-			cb(res);
-		});
-	}
+module.exports = function(sequelize, DataTypes) {
+  var Coffee = sequelize.define("Coffee", {
+    name: DataTypes.STRING,
+    drank: DataTypes.BOOLEAN
+  });
+  return Coffee;
 };
-module.exports = coffee;
